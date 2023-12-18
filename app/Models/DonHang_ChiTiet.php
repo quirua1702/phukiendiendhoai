25005c95c0d7476917ea1706f86666de0d8ae7b6
unknown <nmthuan10@gmail.com>
2023-12-19 00:00:17 +0700

    add code  models don hang chi tiet va nguoi dung

--- app/Models/DonHang_ChiTiet.php
+++ app/Models/DonHang_ChiTiet.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class DonHang_ChiTiet extends Model
 {
     use HasFactory;
+    protected $table = 'donhangchitiet';
+public function DonHang(): BelongsTo
+{
+return $this->belongsTo(DonHang::class, 'donhang_id', 'id');
+}
+public function SanPham(): BelongsTo
+{
+return $this->belongsTo(SanPham::class, 'sanpham_id', 'id');
+}
 }
--- app/Models/NguoiDung.php
+++ app/Models/NguoiDung.php
@@ -7,39 +7,46 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Notifications\Messages\MailMessage;
 
-class User extends Authenticatable
+class NguoiDung extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
+    protected $table = 'nguoidung';
     protected $fillable = [
         'name',
         'email',
         'password',
     ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
-        'password' => 'hashed',
     ];
+    public function DonHang(): HasMany
+    {
+        return $this->hasMany(DonHang::class, 'nguoidung_id', 'id');
+    }
+    public function sendPasswordResetNotification($token)
+    {
+        $this->notify(new CustomResetPasswordNotification($token));
+    }
+}
+class CustomResetPasswordNotification extends ResetPassword
+
+{
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->subject('Khôi phục mật khẩu')
+            ->line('Bạn vừa yêu cầu ' . config('app.name') . ' khôi phục mật khẩu của mình.')
+            ->line('Liên kết đặt lại mật khẩu này sẽ hết hạn sau 60 phút.')
+            ->line('Xin vui lòng nhấn vào nút "Khôi phục mật khẩu" bên dưới để tiến hành cấp mật khẩu mới.')
+            ->action('Khôi phục mật khẩu', url(config('app.url') . route('password.reset', $this->token, false)))
+            ->line('Nếu bạn không yêu cầu đặt lại mật khẩu, xin vui lòng không làm gì thêm và báo lại cho quản trị hệ thống về vấn đề này.');
+    }
 }
