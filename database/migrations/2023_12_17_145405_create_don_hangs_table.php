93c015d48758a92be7e60ea32146ad730ad20741
ngocman0806 <tnman_21th@student.agu.edu.vn>
2023-12-18 17:52:25 +0700

    init project

---
+++ database/migrations/2023_12_17_145405_create_don_hangs_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('donhang', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('tinhtrang_id')->constrained('tinhtrang');
+            $table->foreignId('nguoidung_id')->constrained('nguoidung');
+            $table->string('dienthoaigiaohang', 20);
+            $table->string('diachigiaohang');
+            $table->timestamps();
+            $table->engine = 'InnoDB';
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('donhang');
+    }
+};
