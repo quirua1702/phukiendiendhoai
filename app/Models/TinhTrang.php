298b44c5541097e24c5ddb28dc70b7041390fe7c
unknown <nmthuan10@gmail.com>
2023-12-19 00:11:31 +0700

    add code  models hang san xuat and tinh trang

--- app/Models/TinhTrang.php
+++ app/Models/TinhTrang.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class TinhTrang extends Model
 {
-    use HasFactory;
+    protected $table = 'tinhtrang';
+    public function DonHang(): HasMany
+    {
+    return $this->hasMany(DonHang::class, 'tinhtrang_id', 'id');
+    }
 }
