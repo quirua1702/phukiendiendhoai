5ecb1a6bad9a92a508d59294e95f262393cf11ee
unknown <nmthuan10@gmail.com>
2023-12-18 23:55:00 +0700

    add code  models don hang

--- app/Models/DonHang.php
+++ app/Models/DonHang.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
 
 class DonHang extends Model
 {
     use HasFactory;
+    protected $table = 'donhang';
+    public function NguoiDung(): BelongsTo
+    {
+        return $this->belongsTo(NguoiDung::class, 'nguoidung_id', 'id');
+    }
+    public function TinhTrang(): BelongsTo
+    {
+        return $this->belongsTo(TinhTrang::class, 'tinhtrang_id', 'id');
+    }
+    public function DonHang_ChiTiet(): HasMany
+    {
+        return $this->hasMany(DonHang_ChiTiet::class, 'donhang_id', 'id');
+    }
 }
