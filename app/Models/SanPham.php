7bc3c1c839e23b88ac70f9d66ab9030a5dea21f0
unknown <nmthuan10@gmail.com>
2023-12-18 23:46:42 +0700

    add models San Pham

--- app/Models/SanPham.php
+++ app/Models/SanPham.php
@@ -4,8 +4,34 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class SanPham extends Model
 {
-    use HasFactory;
+    protected $table = 'sanpham';
+
+    protected $fillable = [
+        'loaisanpham_id',
+        'hangsanxuat_id',
+        'tensanpham',
+        'tensanpham_slug',
+        'soluong',
+        'dongia',
+        'hinhanh',
+        'motasanpham',
+        ];
+    
+    public function LoaiSanPham(): BelongsTo
+    {
+    return $this->belongsTo(LoaiSanPham::class, 'loaisanpham_id', 'id');
+    }
+    public function HangSanXuat(): BelongsTo
+    {
+    return $this->belongsTo(HangSanXuat::class, 'hangsanxuat_id', 'id');
+    }
+    public function DonHang_ChiTiet(): HasMany
+    {
+    return $this->hasMany(DonHang_ChiTiet::class, 'sanpham_id', 'id');
+    }
 }
