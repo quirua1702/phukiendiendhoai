80996e953e1de577d6c7b8a2b4f2553e67528ffc
quirua1702 <152962491+quirua1702@users.noreply.github.com>
2023-12-19 12:04:23 +0700

    Update HangSanXuat.php
    Signed-off-by: quirua1702 <152962491+quirua1702@users.noreply.github.com>

--- app/Models/HangSanXuat.php
+++ app/Models/HangSanXuat.php
@@ -1,22 +1,17 @@
 <?php
-
 namespace App\Models;
-
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-
 class HangSanXuat extends Model
 {
-    protected $table = 'hangsanxuat';
-    protected $fillable = [
-        'tenhang',
-        'tenhang_slug',
-        'hinhanh',
-
+protected $table = 'hangsanxuat';
+protected $fillable = [
+    'tenhang',
+    'tenhang_slug',
+    'hinhanh',
     ];
-    public function SanPham(): HasMany
-    {
-        return $this->hasMany(SanPham::class, 'hangsanxuat_id', 'id');
-    }
+public function SanPham(): HasMany
+{
+return $this->hasMany(SanPham::class, 'hangsanxuat_id', 'id');
+}
 }
