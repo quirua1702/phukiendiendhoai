8efc8187949d9a8e38bd562b1756a1ba0154878b
quirua1702 <152962491+quirua1702@users.noreply.github.com>
2023-12-19 11:58:18 +0700

    Update KhachHangController.php
    Signed-off-by: quirua1702 <152962491+quirua1702@users.noreply.github.com>

--- app/Http/Controllers/KhachHangController.php
+++ app/Http/Controllers/KhachHangController.php
@@ -5,14 +5,14 @@ namespace App\Http\Controllers;
 use App\Models\NguoiDung;
 use App\Models\DonHang;
 use App\Models\DonHang_ChiTiet;
-use App\Models\User;
-use App\Mail\DatHangThanhCongEmail;
+use App\Mail\DatHangEmail;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
 use Gloudemans\Shoppingcart\Facades\Cart;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Mail;
+
 
 class KhachHangController extends Controller
 {
@@ -20,29 +20,28 @@ class KhachHangController extends Controller
     {
         if(Auth::check())
         {
-            $nguoidung = User::find(Auth::user()->id);
-            return view('user.home', compact('nguoidung'));
+        $nguoidung = NguoiDung::find(Auth::user()->id);
+        return view('user.home', compact('nguoidung'));
         }
         else
-            return redirect()->route('user.dangnhap');
+        return redirect()->route('user.dangnhap');
     }
 
     public function getDatHang()
     {
         if(Auth::check())
-            return view('user.dathang');
+        return view('user.dathang');
         else
-            return redirect()->route('user.dangnhap');
+        return redirect()->route('user.dangnhap');
     }
 
     public function postDatHang(Request $request)
     {
-        // Kiểm tra
+// Kiểm tra
         $this->validate($request, [
-            'diachigiaohang' => ['required', 'string', 'max:255'],
-            'dienthoaigiaohang' => ['required', 'string', 'max:255'],
+        'diachigiaohang' => ['required', 'string', 'max:255'],
+        'dienthoaigiaohang' => ['required', 'string', 'max:255'],
         ]);
-    
         // Lưu vào đơn hàng
         $dh = new DonHang();
         $dh->nguoidung_id = Auth::user()->id;
@@ -50,36 +49,32 @@ class KhachHangController extends Controller
         $dh->diachigiaohang = $request->diachigiaohang;
         $dh->dienthoaigiaohang = $request->dienthoaigiaohang;
         $dh->save();
-
         // Lưu vào đơn hàng chi tiết
         foreach(Cart::content() as $value)
         {
-            $ct = new DonHang_ChiTiet();
-            $ct->donhang_id = $dh->id;
-            $ct->sanpham_id = $value->id;
-            $ct->soluongban = $value->qty;
-            $ct->dongiaban = $value->price;
-            $ct->save();
+        $ct = new DonHang_ChiTiet();
+        $ct->donhang_id = $dh->id;
+        $ct->sanpham_id = $value->id;
+        $ct->soluongban = $value->qty;
+        $ct->dongiaban = $value->price;
+        $ct->save();
         }
-        
-        //Gởi mail
-        try
-        {
-            Mail::to(Auth::user()->email)->send(new DatHangThanhCongEmail($dh));
-        }
-        catch (Exception $e)
-        {
-            return $e->getMessage();
-        }
+        //gởi mail
+        try{
+            Mail::to(Auth::user()->email)->send(new DatHangEmail($dh));
 
+        }
+        catch(Exception $e)
+        {
+            return $e ->getMessage();
+        }
         return redirect()->route('user.dathangthanhcong');
     }
 
     public function getDatHangThanhCong()
     {
-        // Xóa giỏ hàng khi hoàn tất đặt hàng?
+// Xóa giỏ hàng khi hoàn tất đặt hàng?
         Cart::destroy();
-        
         return view('user.dathangthanhcong');
     
     }
@@ -96,28 +91,25 @@ class KhachHangController extends Controller
 
     public function getHoSoCaNhan()
     {
+        // Bổ sung code tại đây
         return redirect()->route('user.home');
     }
 
     public function postHoSoCaNhan(Request $request)
     {
         $id = Auth::user()->id;
-
         $request->validate([
-            'name' => ['required', 'string', 'max:100'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users,email,' . $id],
-            'password' => ['confirmed'],
+        'name' => ['required', 'string', 'max:100'],
+        'email' => ['required', 'string', 'email', 'max:255', 'unique:users,email,' . $id],
+        'password' => ['confirmed'],
         ]);
-
         $orm = User::find($id);
         $orm->name = $request->name;
         $orm->username = Str::before($request->email, '@');
         $orm->email = $request->email;
         if(!empty($request->password)) $orm->password = Hash::make($request->password);
         $orm->save();
-        
         return redirect()->route('user.home')->with('success', 'Đã cập nhật thông tin thành công.');
-        
     }
 
     public function postDangXuat(Request $request)
