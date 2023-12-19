19d4a06a14d63b7b5f85674e0a4e006aa1301815
quirua1702 <152962491+quirua1702@users.noreply.github.com>
2023-12-19 11:57:47 +0700

    Update HomeController.php
    Signed-off-by: quirua1702 <152962491+quirua1702@users.noreply.github.com>

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,6 +15,10 @@ use Gloudemans\Shoppingcart\Facades\Cart;
 use Socialite;
 class HomeController extends Controller
 {
+
+
+
+
     /**
      * Create a new controller instance.
      *
@@ -33,7 +37,6 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-   
     public function getHome()
     {
         $loaisanpham = LoaiSanPham::all();
@@ -42,7 +45,7 @@ class HomeController extends Controller
 
     public function getSanPham($tenloai_slug = '')
     {
-        // Bổ sung code tại đây
+    // Bổ sung code tại đây
         return view('frontend.sanpham');
     }
 
@@ -56,22 +59,22 @@ class HomeController extends Controller
     {
         if(empty($tenchude_slug))
         {
-            $title = 'Bài viết';
-            $baiviet = BaiViet::where('kichhoat', 1)
-                ->where('kiemduyet', 1)
-                ->orderBy('created_at', 'desc')
-                ->paginate(20);
+        $title = 'Bài viết';
+        $baiviet = BaiViet::where('kichhoat', 1)
+        ->where('kiemduyet', 1)
+        ->orderBy('created_at', 'desc')
+        ->paginate(20);
         }
         else
         {
-            $chude = ChuDe::where('tenchude_slug', $tenchude_slug)
-                ->firstOrFail();
-            $title = $chude->tenchude;
-            $baiviet = BaiViet::where('kichhoat', 1)
-                ->where('kiemduyet', 1)
-                ->where('chude_id', $chude->id)
-                ->orderBy('created_at', 'desc')
-                ->paginate(20);
+        $chude = ChuDe::where('tenchude_slug', $tenchude_slug)
+        ->firstOrFail();
+        $title = $chude->tenchude;
+        $baiviet = BaiViet::where('kichhoat', 1)
+        ->where('kiemduyet', 1)
+        ->where('chude_id', $chude->id)
+        ->orderBy('created_at', 'desc')
+        ->paginate(20);
         }
         return view('frontend.baiviet', compact('title', 'baiviet'));
     }
@@ -81,54 +84,50 @@ class HomeController extends Controller
         $tieude_id = explode('.', $tieude_slug);
         $tieude = explode('-', $tieude_id[0]);
         $baiviet_id = $tieude[count($tieude) - 1];
-
         $baiviet = BaiViet::where('kichhoat', 1)
-            ->where('kiemduyet', 1)
-            ->where('id', $baiviet_id)
-            ->firstOrFail();
-            
+        ->where('kiemduyet', 1)
+        ->where('id', $baiviet_id)
+        ->firstOrFail();
         if(!$baiviet) abort(404);
-
         // Cập nhật lượt xem
         $daxem = 'BV' . $baiviet_id;
         if(!session()->has($daxem))
         {
-            $orm = BaiViet::find($baiviet_id);
-            $orm->luotxem = $baiviet->luotxem + 1;
-            $orm->save();
-            session()->put($daxem, 1);
+        $orm = BaiViet::find($baiviet_id);
+        $orm->luotxem = $baiviet->luotxem + 1;
+        $orm->save();
+        session()->put($daxem, 1);
         }
         $baivietcungchuyemuc = BaiViet::where('kichhoat', 1)
-            ->where('kiemduyet', 1)
-            ->where('chude_id', $baiviet->chude_id)
-            ->where('id', '!=', $baiviet_id)
-            ->orderBy('created_at', 'desc')
-            ->take(4)->get();
-            
+        ->where('kiemduyet', 1)
+        ->where('chude_id', $baiviet->chude_id)
+        ->where('id', '!=', $baiviet_id)
+        ->orderBy('created_at', 'desc')
+        ->take(4);
         return view('frontend.baiviet_chitiet', compact('baiviet', 'baivietcungchuyemuc'));
     }
 
     public function getGioHang()
     {
+        // Bổ sung code tại đây
         if(Cart::count() > 0)
-            return view('frontend.giohang');
+        return view('frontend.giohang');
         else
-            return view('frontend.giohangrong');
+        return view('frontend.giohangrong');
     }
 
     public function getGioHang_Them($tensanpham_slug = '')
     {
         $sanpham = SanPham::where('tensanpham_slug', $tensanpham_slug)->first();
-
         Cart::add([
-            'id' => $sanpham->id,
-            'name' => $sanpham->tensanpham,
-            'price' => $sanpham->dongia,
-            'qty' => 1,
-            'weight' => 0,
-            'options' => [
-                'image' => $sanpham->hinhanh
-            ]
+        'id' => $sanpham->id,
+        'name' => $sanpham->tensanpham,
+        'price' => $sanpham->dongia,
+        'qty' => 1,
+        'weight' => 0,
+        'options' => [
+        'image' => $sanpham->hinhanh
+        ]
         ]);
         return redirect()->route('frontend.home');
     }
@@ -142,11 +141,10 @@ class HomeController extends Controller
     public function getGioHang_Giam($row_id)
     {
         $row = Cart::get($row_id);
-
         // Nếu số lượng là 1 thì không giảm được nữa
         if($row->qty > 1)
         {
-            Cart::update($row_id, $row->qty - 1);
+        Cart::update($row_id, $row->qty - 1);
         }
         return redirect()->route('frontend.giohang');
     }
@@ -154,11 +152,10 @@ class HomeController extends Controller
     public function getGioHang_Tang($row_id)
     {
         $row = Cart::get($row_id);
-
         // Không được tăng vượt quá 10 sản phẩm
         if($row->qty < 10)
         {
-            Cart::update($row_id, $row->qty + 1);
+        Cart::update($row_id, $row->qty + 1);
         }
         return redirect()->route('frontend.giohang');
     }
@@ -168,12 +165,13 @@ class HomeController extends Controller
         foreach($request->qty as $row_id => $quantity)
         {
         if($quantity <= 0)
-            Cart::update($row_id, 1);
+        Cart::update($row_id, 1);
         else if($quantity > 10)
-            Cart::update($row_id, 10);
+        Cart::update($row_id, 10);
         else
-            Cart::update($row_id, $quantity);
+        Cart::update($row_id, $quantity);
         }
+        
         return redirect()->route('frontend.giohang');
     }
 
@@ -202,7 +200,6 @@ class HomeController extends Controller
     {
         return Socialite::driver('google')->redirect();
     }
-
     public function getGoogleCallback()
     {
         try
@@ -216,28 +213,61 @@ class HomeController extends Controller
         {
             return redirect()->route('user.dangnhap')->with('warning', 'Lỗi xác thực. Xin vui lòng thử lại!');
         }
-        $existingUser = NguoiDung::where('email', $user->email)->first();
-        if($existingUser)
+            $existingUser = NguoiDung::where('email', $user->email)->first();
+            if($existingUser)
         {
-            // Nếu người dùng đã tồn tại thì đăng nhập
-            Auth::login($existingUser, true);
-            return redirect()->route('user.home');
+        // Nếu người dùng đã tồn tại thì đăng nhập
+        Auth::login($existingUser, true);
+        return redirect()->route('user.home');
         }
         else
         {
             // Nếu chưa tồn tại người dùng thì thêm mới
             $newUser = NguoiDung::create([
-                'name' => $user->name,
-                'email' => $user->email,
-                'username' => Str::before($user->email, '@'),
-                'password' => Hash::make('larashop@2023'), // Gán mật khẩu tự do
-            ]);
-            // Sau đó đăng nhập
-            Auth::login($newUser, true);
-            return redirect()->route('user.home');
+            'name' => $user->name,
+            'email' => $user->email,
+            'username' => Str::before($user->email, '@'),
+            'password' => Hash::make('larashop@2023'), // Gán mật khẩu tự do
+        ]);
+        // Sau đó đăng nhập
+        Auth::login($newUser, true);
+        return redirect()->route('user.home');
         }
     }
-
 }
 
+
+
+    /*
+    public function getDatHangDemo() 
+    { 
+        // Thêm Đơn hàng demo 
+        $donhang = new DonHang(); 
+        $donhang->user_id = Auth::user()->id; 
+        $donhang->tinhtrang_id = 1; // Lưu ý: Bảng tinhtrang phải có dữ liệu có id = 1 
+        $donhang->dienthoaigiaohang = '0939011900'; 
+        $donhang->diachigiaohang = '18 Ung Văn Khiêm - TP. Long Xuyên - An Giang'; 
+        $donhang->save(); 
+        
+        // Thêm Đơn hàng chi tiết demo 
+        $donhang_chitiet = new DonHang_ChiTiet(); 
+        $donhang_chitiet->donhang_id = $donhang->id;
+        $donhang_chitiet->sanpham_id = 1; // Lưu ý: Bảng sanpham phải có dữ liệu có id = 1 
+        $donhang_chitiet->soluongban = 1; 
+        $donhang_chitiet->dongiaban = 5990000; 
+        $donhang_chitiet->save();
+
+        $donhang_chitiet = new DonHang_ChiTiet(); 
+        $donhang_chitiet->donhang_id = $donhang->id; 
+        $donhang_chitiet->sanpham_id = 2; // Lưu ý: Bảng sanpham phải có dữ liệu có id = 2 
+        $donhang_chitiet->soluongban = 1; 
+        $donhang_chitiet->dongiaban = 350000; 
+        $donhang_chitiet->save(); 
+        
+        // Gởi email 
+        Mail::to(Auth::user()->email)->send(new DatHangEmail($donhang)); 
+        
+        return redirect()->route('frontend.dathangthanhcong');
+    }
+    */
 
