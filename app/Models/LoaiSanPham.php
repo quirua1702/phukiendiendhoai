ba5fce6119a1fd1e54143b037a666c990200638e
quirua1702 <152962491+quirua1702@users.noreply.github.com>
2023-12-19 12:04:47 +0700

    Update LoaiSanPham.php
    Signed-off-by: quirua1702 <152962491+quirua1702@users.noreply.github.com>

--- app/Models/LoaiSanPham.php
+++ app/Models/LoaiSanPham.php
@@ -1,14 +1,10 @@
 <?php
-
 namespace App\Models;
-
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-
 class LoaiSanPham extends Model
 {
-    protected $table = 'loaisanpham';
+protected $table = 'loaisanpham';
 public function SanPham(): HasMany
 {
 return $this->hasMany(SanPham::class, 'loaisanpham_id', 'id');
